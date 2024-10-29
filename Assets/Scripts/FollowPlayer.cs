7e302a17c5c68fd42b3414973fcda6c9e17a9985
Nangil <surasit.dang@bumail.net>
2024-10-30 04:08:44 +0700

    SuSu

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player; // อ้างอิงถึงตำแหน่งของ Player
+    public Vector3 offset; // ใช้สำหรับกำหนดระยะห่างจาก Player
+
+    void Update()
+    {
+        if (player != null)
+        {
+            // ปรับตำแหน่งของวัตถุให้ตามตำแหน่งของ Player โดยเพิ่มระยะ offset
+            transform.position = player.position + offset;
+        }
+    }
+}
