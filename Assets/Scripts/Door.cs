b8a6d095c2e06b25712f9d0368f0c7ce39ca7a30
louzixgod <148941909+Jutrathip@users.noreply.github.com>
2024-10-29 20:12:15 +0700

    Map2

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // ใช้ SceneManager
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private string sceneToLoad; // ชื่อฉากที่ต้องการเปลี่ยนไป
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // ตรวจสอบว่าชนกับวัตถุที่มีแท็ก "Player" หรือไม่ (สามารถเปลี่ยนตามต้องการ)
+        if (other.CompareTag("Player"))
+        {
+            // เปลี่ยนไปยังฉากที่ระบุ
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
